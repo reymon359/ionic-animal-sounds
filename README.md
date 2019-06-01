f897bab76e627dfb63c177dc57464aab5ee245c3
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-01 23:43:05 +0200

    Publishing to github pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# ionic-animal-sounds
-A simple ionic app to test sounds in ionic and ordering ionic lists
--- src/interfaces/animal.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Animal {
-  nombre:string;
-imagen:string;
-audio:string;
-duracion:number;
-reproduciendo:boolean;
-}
